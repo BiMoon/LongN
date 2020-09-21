d052f92f61c774df3fa29ca37683e4ad13ce11e6
BiMoon <71074089+BiMoon@users.noreply.github.com>
2020-09-21 14:13:45 +0700

    Add files via upload
    re-up

--- Selenium/Test_run/src/test/java/testDemo/GoogleHomepageTests.java
+++ Selenium/Test_run/src/test/java/testDemo/GoogleHomepageTests.java
@@ -2,7 +2,11 @@ package testDemo;
 
 //import org.testng.annotations.Test;
 import framework.core.BaseTest;
-//import pageobject.GoogleHomepage;
+import pageobject.GoogleHomepage;
+
+import static org.junit.Assert.*;
+import static org.testng.Assert.assertEquals;
+
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
  
@@ -10,23 +14,49 @@ import org.openqa.selenium.WebDriver;
 public class GoogleHomepageTests extends BaseTest{
 		static WebDriver webdriver;
 		static String url = "http://www.google.com";
+		
+		
+		@Before
+		public void setUp()  {
+		
+		
+		}
+		
+		
+			
+        @Test
+        public void googlesearchEdge() throws Exception {
+        	String key = "JAVA + SELENIUM";
 
-		@Test
+        	GoogleHomepage googleHomepage = new GoogleHomepage(getDriver("E", url)); ; // = new GoogleHomepage(getDriver("F", url)); // "F" with under Firefox browser
+        	
+            System.out.print("Open " + url + " in MS Edge");
+            
+            
+			googleHomepage.searhTestinput(key);
+        }
+
+		//@Test
+		
         public void homepageTestsFirefox() throws Exception {
-			//GoogleHomepage googleHomepage = new GoogleHomepage(getDriver("E", url));
-			BaseTest.getDriver("F", url);
+			GoogleHomepage googleHomepage = new GoogleHomepage(getDriver("E", url));
+			
             System.out.print("Open " + url + " in Firefox");
         }
-        @Test
+
+        //@Test
         public void homepageTestsMsEdge() throws Exception {
             //GoogleHomepage googleHomepage = new GoogleHomepage(getDriver("E", url));
-        	BaseTest.getDriver("E", url);
+        	
             System.out.print("Open " + url + " in MS Edge");
         }
+            
+
         @After
 		public void tearDown() throws Exception {
 		// Close the browser
-		//Thread.sleep(3);
+		Thread.sleep(3000);
+		
 		quitDriver();
 		
 	}
